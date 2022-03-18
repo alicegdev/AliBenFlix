c6aad10d16b44661793b44e66f0e14cf5a0a8bd9
alicegdev <71830893+alicegdev@users.noreply.github.com>
2022-03-18 15:21:15 +0100

    login fonctionne avec PDO

--- model/HomeModel.php
+++ model/HomeModel.php
@@ -6,34 +6,16 @@ class HomeModel
 
     public function checkUserLogin($email, $password)
     {
-        // avec mysqli_query
-        $mysqli = new mysqli("localhost", "root", "", "alibenflix");
-
-        // Check connection
-        if ($mysqli->connect_errno) {
-            echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-            exit();
-        }
-
-        $query = "SELECT * FROM user WHERE email='$email' AND user_password='$password'";
-        $results = mysqli_query($mysqli, $query);
-
-        if (mysqli_num_rows($results) == 1) {
-            return mysqli_num_rows($results);
-        }
-
-
-
         // avec PDO
-        // try {
-        //     $query = "SELECT if(COUNT(*)>0,'true','false') FROM alibenflix.user WHERE user.email='{$email}' AND user.password='{$password}'";
-        //     $stmt = $this->db->prepare($query);
-        //     $stmt->execute();
-        //     return $stmt;
-        // } catch (\PDOException $e) {
-        //     echo $e->getMessage();
-        //     exit;
-        // }
+        try {
+            $query = "SELECT COUNT(*) FROM alibenflix.user WHERE user.email='{$email}' AND user.user_password='{$password}'";
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchColumn();
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            exit;
+        }
     }
 
     public function userRegister($nom, $prenom, $email, $password)

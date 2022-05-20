990668f1f2c145b2b73c35151200dc8961c62201
alicegdev <71830893+alicegdev@users.noreply.github.com>
2022-05-20 16:04:56 +0200

    logo et commentaire passage macOS

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
 
-// A modifier pour passage sur MacOS
 return $config  = [
     'server' => 'localhost',
     'dbname' => 'alibenflix',
     'dbuser' => 'root',
-    'dbpass' => 'root'
+    // A COMMENTER SI passage sur Mac OS
+    'dbpass' => ''
+    // A DECOMMENETER SI passage sur MacOS
+    // 'dbpass' => 'root'
 ];
